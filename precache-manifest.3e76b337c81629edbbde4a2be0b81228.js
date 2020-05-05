4576398a87c62f23508d23a222e13695e30ef065
Apple <apple@Apples-MacBook-Pro.local>
2020-05-05 08:47:59 +0530

    Updates

---
+++ precache-manifest.3e76b337c81629edbbde4a2be0b81228.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bbd8f035f73e832b29d5986b2abd721",
+    "url": "/RecipeApp_react/index.html"
+  },
+  {
+    "revision": "99de9b4aa24042817009",
+    "url": "/RecipeApp_react/static/css/main.c213a077.chunk.css"
+  },
+  {
+    "revision": "c807c3953791c3553d33",
+    "url": "/RecipeApp_react/static/js/2.abf01545.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/RecipeApp_react/static/js/2.abf01545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99de9b4aa24042817009",
+    "url": "/RecipeApp_react/static/js/main.f0d73425.chunk.js"
+  },
+  {
+    "revision": "f9747987f89fbd335a1d",
+    "url": "/RecipeApp_react/static/js/runtime-main.a9977906.js"
+  }
+]);
